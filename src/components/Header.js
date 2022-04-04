6a876b68ff95ecd325a74017e5d86950738bac1b
dev-elixir <abhisri1997@gmail.com>
2022-04-04 07:29:11 +0530

    UI fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,104 +1,111 @@
 import React from "react";
 import "../css/header.css";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="wrapper">
-      <div className="header-content">
-        <div className="header-title">
-          We provide the best solution within the best deliverable time
-          possible. This is a professional association for research assistance
-          and content writing services in the field of science, arts, social
-          sciences, education, engineering etc. With an experienced team and
-          access to multiple resources and tools we guarantee quality work
-          completed in accordance with the provided instructions and time frame
-        </div>
-        <div className="header-form">
-          <div className="card">
-            <p className="card-title">Price Calculator</p>
-            <form className="main-form">
-              <div className="form-group">
-                <label htmlFor="name"></label>
-                <input
-                  type="text"
-                  name="name"
-                  id=""
-                  placeholder="Name"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email"></label>
-                <input
-                  type="email"
-                  name="email"
-                  id=""
-                  placeholder="someone@email.com"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="phone"></label>
-                <input
-                  type="tel"
-                  name="phone"
-                  id=""
-                  placeholder="+91 91345 12345"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="Country"></label>
-                <input
-                  type="text"
-                  name="country"
-                  id=""
-                  placeholder="Country"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group form-flex">
-                <select name="paper" id="" className="form-control">
-                  <option value="" disabled selected>
-                    Select Paper Type
-                  </option>
-                </select>
-                <select name="standard" id="" className="form-control">
-                  <option value="" disabled selected>
-                    Writing Standard
-                  </option>
-                </select>
-              </div>
-              <div className="form-group form-flex">
-                <select name="deadline" id="" className="form-control">
-                  <option value="" disabled selected>
-                    Project Deadline
-                  </option>
-                </select>
-                <select name="pages" id="" className="form-control">
-                  <option value="" disabled selected>
-                    Number of Pages
-                  </option>
-                </select>
-              </div>
-              <div className="form-group promo form-flex">
-                <label htmlFor="promo"></label>
-                <input
-                  type="text"
-                  name="promo"
-                  id=""
-                  placeholder="Promo Code"
-                  className="form-control"
-                />
-                <button className="promo-apply">Apply</button>
-              </div>
-              <p>Original Price : $</p>
-              <p>Discounted Price : $</p>
-              <button type="submit" className="form-control">
-                Book Now
-              </button>
-            </form>
-          </div>{" "}
+    <div className="no-wrapper header">
+      <div className="wrapper">
+        <div className="header-content">
+          <div className="header-title">
+            <span className="quote-icon">
+              <FaQuoteLeft />
+            </span>
+            We provide the best solution within the best deliverable time
+            possible. This is a professional association for research assistance
+            and content writing services in the field of science, arts, social
+            sciences, education, engineering etc. With an experienced team and
+            access to multiple resources and tools we guarantee quality work
+            completed in accordance with the provided instructions and time
+            frame.
+          </div>
+          <div className="header-form">
+            <div className="card">
+              <p className="card-title">Price Calculator</p>
+              <form className="main-form">
+                <div className="form-group">
+                  <label htmlFor="name"></label>
+                  <input
+                    type="text"
+                    name="name"
+                    id=""
+                    placeholder="Name"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email"></label>
+                  <input
+                    type="email"
+                    name="email"
+                    id=""
+                    placeholder="someone@email.com"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone"></label>
+                  <input
+                    type="tel"
+                    name="phone"
+                    id=""
+                    placeholder="+91 91345 12345"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Country"></label>
+                  <input
+                    type="text"
+                    name="country"
+                    id=""
+                    placeholder="Country"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group form-flex">
+                  <select name="paper" id="" className="form-control">
+                    <option value="" disabled selected>
+                      Select Paper Type
+                    </option>
+                  </select>
+                  <select name="standard" id="" className="form-control">
+                    <option value="" disabled selected>
+                      Writing Standard
+                    </option>
+                  </select>
+                </div>
+                <div className="form-group form-flex">
+                  <select name="deadline" id="" className="form-control">
+                    <option value="" disabled selected>
+                      Project Deadline
+                    </option>
+                  </select>
+                  <select name="pages" id="" className="form-control">
+                    <option value="" disabled selected>
+                      Number of Pages
+                    </option>
+                  </select>
+                </div>
+                <div className="form-group promo form-flex">
+                  <label htmlFor="promo"></label>
+                  <input
+                    type="text"
+                    name="promo"
+                    id=""
+                    placeholder="Promo Code"
+                    className="form-control"
+                  />
+                  <button className="promo-apply">Apply</button>
+                </div>
+                <p>Original Price : $</p>
+                <p>Discounted Price : $</p>
+                <button type="submit" className="form-control">
+                  Book Now
+                </button>
+              </form>
+            </div>{" "}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import nav from "../data/nav";
 import "../css/nav.css";
@@ -6,31 +6,46 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   const [isToggled, setIsToggled] = useState(false);
+  const [fixedNav, setFixedNav] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", navbarFixed);
+    return () => window.removeEventListener("scroll", navbarFixed);
+  }, []);
+
+  const navbarFixed = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      windowHeight > 100 ? setFixedNav("fixed-nav") : setFixedNav("");
+    }
+  };
   return (
-    <div className="wrapper">
-      <nav className="main-nav">
-        <div className="mobile-nav">
-          <div className="logo">
-            <h1>Projectshala</h1>
+    <div className={`no-wrapper nav ${fixedNav}`}>
+      <div className="wrapper">
+        <nav className="main-nav">
+          <div className="mobile-nav">
+            <div className="logo">
+              <h1>Projectshala</h1>
+            </div>
+            <div className="menu-icon">
+              <FiMenu
+                className="icons"
+                onClick={() => setIsToggled(!isToggled)}
+              />
+            </div>
           </div>
-          <div className="menu-icon">
-            <FiMenu
-              className="icons"
-              onClick={() => setIsToggled(!isToggled)}
-            />
-          </div>
-        </div>
-        <ul className={isToggled ? "mobile-links" : "links-list"}>
-          {nav.map((link) => {
-            const { id, text, url } = link;
-            return (
-              <li key={id}>
-                <Link to={url}>{text}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+          <ul className={isToggled ? "mobile-links" : "links-list"}>
+            {nav.map((link) => {
+              const { id, text, url } = link;
+              return (
+                <li key={id} className="nav-links">
+                  <Link to={url}>{text}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
